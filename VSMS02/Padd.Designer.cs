a8247882c5860123bb12d333f730efdd04767de4
Jessi Sibayan <jessisibayan@gmail.com>
2017-10-17 21:55:11 +0800

    Added adding of patient details

--- VSMS02/Padd.Designer.cs
+++ VSMS02/Padd.Designer.cs
@@ -28,20 +28,315 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dteBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboGender = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTelephoneNumber = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dteDateAdmitted = new System.Windows.Forms.DateTimePicker();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtPhilhealthNumber = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First name";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(12, 29);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(140, 20);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.Location = new System.Drawing.Point(158, 29);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(140, 20);
+            this.txtMiddleName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Middle name";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(304, 29);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(140, 20);
+            this.txtLastName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Birthdate";
+            // 
+            // dteBirthDate
+            // 
+            this.dteBirthDate.Location = new System.Drawing.Point(12, 77);
+            this.dteBirthDate.Name = "dteBirthDate";
+            this.dteBirthDate.Size = new System.Drawing.Size(200, 20);
+            this.dteBirthDate.TabIndex = 7;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(218, 77);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(99, 20);
+            this.txtAge.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(219, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Age";
+            // 
+            // cboGender
+            // 
+            this.cboGender.FormattingEnabled = true;
+            this.cboGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cboGender.Location = new System.Drawing.Point(323, 77);
+            this.cboGender.Name = "cboGender";
+            this.cboGender.Size = new System.Drawing.Size(121, 21);
+            this.cboGender.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(320, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Gender";
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.Location = new System.Drawing.Point(304, 129);
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.Size = new System.Drawing.Size(140, 20);
+            this.txtContactName.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(305, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(148, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Contact in case of emergency";
+            // 
+            // txtTelephoneNumber
+            // 
+            this.txtTelephoneNumber.Location = new System.Drawing.Point(158, 129);
+            this.txtTelephoneNumber.Name = "txtTelephoneNumber";
+            this.txtTelephoneNumber.Size = new System.Drawing.Size(140, 20);
+            this.txtTelephoneNumber.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(159, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Telephone number";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(12, 129);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(140, 20);
+            this.txtPhoneNumber.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 113);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Phone number";
+            // 
+            // dteDateAdmitted
+            // 
+            this.dteDateAdmitted.Location = new System.Drawing.Point(12, 225);
+            this.dteDateAdmitted.Name = "dteDateAdmitted";
+            this.dteDateAdmitted.Size = new System.Drawing.Size(200, 20);
+            this.dteDateAdmitted.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 208);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Date admitted";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 297);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(116, 23);
+            this.btnAdd.TabIndex = 23;
+            this.btnAdd.Text = "Add Patient Info";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtPhilhealthNumber
+            // 
+            this.txtPhilhealthNumber.Location = new System.Drawing.Point(12, 175);
+            this.txtPhilhealthNumber.Name = "txtPhilhealthNumber";
+            this.txtPhilhealthNumber.Size = new System.Drawing.Size(140, 20);
+            this.txtPhilhealthNumber.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 159);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(93, 13);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "PhilHealth number";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(131, 297);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(114, 23);
+            this.btnCancel.TabIndex = 25;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(158, 175);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(286, 20);
+            this.txtAddress.TabIndex = 27;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(159, 159);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(45, 13);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "Address";
             // 
             // Padd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(461, 332);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtPhilhealthNumber);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dteDateAdmitted);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtContactName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtTelephoneNumber);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cboGender);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dteBirthDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label1);
             this.Name = "Padd";
             this.Text = "Add Patient";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Padd_FormClosing);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dteBirthDate;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboGender;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtContactName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTelephoneNumber;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dteDateAdmitted;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtPhilhealthNumber;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label12;
     }
 }
--- VSMS02/Padd.cs
+++ VSMS02/Padd.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,53 @@ namespace VSMS02
         private void Padd_FormClosing(object sender, FormClosingEventArgs e)
         {
             System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string projDir = Directory.GetParent(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).FullName).FullName;
+            string dbDir = projDir + "\\VSMS.mdf";
+            string connString2 = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + dbDir + ";Integrated Security=True";
+
+
+            string firstName = txtFirstName.Text;
+            string middleName = txtMiddleName.Text;
+            string lastName = txtLastName.Text;
+            string birthDate = dteBirthDate.Text;
+            string address = txtAddress.Text;
+            string gender = cboGender.SelectedItem.ToString();
+            string age = txtAge.Text;
+            string phoneNumber = txtPhoneNumber.Text;
+            string telephoneNumber = txtTelephoneNumber.Text;
+            string contactName = txtContactName.Text;
+            string philhealthNumber = txtPhilhealthNumber.Text;
+            string dateAdmitted = dteDateAdmitted.Text;
+
+
+            using (SqlConnection connection = new SqlConnection(connString2))
+            {
+                string query = "INSERT into Patient (FirstName,MiddleName,LastName,BirthDate,Address,Gender,Age,PhoneNumber,TelephoneNumber,ContactName,PhilhealthNumber,DateAdmitted)" +
+                    " VALUES (@firstName,@middleName,@lastName,@birthDate,@address,@gender,@age,@phoneNumber,@telephoneNumber,@contactName,@philhealthNumber,@dateAdmitted)";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Connection = connection;
+                    cmd.Parameters.Add("@firstName", SqlDbType.NVarChar).Value = firstName;
+                    cmd.Parameters.Add("@middleName", SqlDbType.NVarChar).Value = middleName;
+                    cmd.Parameters.Add("@lastName", SqlDbType.NVarChar).Value = lastName;
+                    cmd.Parameters.Add("@birthDate", SqlDbType.DateTime).Value = birthDate;
+                    cmd.Parameters.Add("@address", SqlDbType.NVarChar).Value = address;
+                    cmd.Parameters.Add("@gender", SqlDbType.NVarChar).Value = gender;
+                    cmd.Parameters.Add("@age", SqlDbType.Int).Value = Int32.Parse(age);
+                    cmd.Parameters.Add("@phoneNumber", SqlDbType.NVarChar).Value = phoneNumber;
+                    cmd.Parameters.Add("@telephoneNumber", SqlDbType.NVarChar).Value = telephoneNumber;
+                    cmd.Parameters.Add("@contactName", SqlDbType.NVarChar).Value = contactName;
+                    cmd.Parameters.Add("@philhealthNumber", SqlDbType.NVarChar).Value = philhealthNumber;
+                    cmd.Parameters.Add("@dateAdmitted", SqlDbType.DateTime).Value = dateAdmitted;
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
